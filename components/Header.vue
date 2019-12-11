dba15899aa8dff176938f173b88a876f4b210258
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-12-11 18:03:16 +0100

    💄 header active / new

--- components/Header.vue
+++ components/Header.vue
@@ -5,22 +5,22 @@
         </nuxt-link>
         <ul class="menu">
             <li>
-                <a href="http://www.winpharma.com/">Winpharma</a>
+                <a href="http://www.winpharma.com/"><span class="txt">Winpharma</span></a>
             </li>
             <li class="active">
-                <nuxt-link to="/">winAutopilote</nuxt-link>
+                <nuxt-link to="/"><span class="new">new</span> <span class="txt">winAutopilote</span></nuxt-link>
             </li>
             <li>
-                <a href="http://www.winpharma.com/temoignages/">Témoignages</a>
+                <a href="http://www.winpharma.com/temoignages/"><span class="txt">Témoignages</span></a>
             </li>
             <li>
-                <a href="http://www.winpharma.com/actualites/">Actualités</a>
+                <a href="http://www.winpharma.com/actualites/"><span class="txt">Actualités</span></a>
             </li>
             <li>
-                <a href="http://www.winpharma.com/recrutement/">Recrutement</a>
+                <a href="http://www.winpharma.com/recrutement/"><span class="txt">Recrutement</span></a>
             </li>
             <li>
-                <a href="http://www.winpharma.com/contact/">Contact</a>
+                <a href="http://www.winpharma.com/contact/"><span class="txt">Contact</span></a>
             </li>
         </ul>
     </header>
@@ -60,11 +60,47 @@ export default {};
     > li {
         margin-left: 50px;
         &.active {
+            a {
+                .txt {
+                    &::before {
+                        opacity: 1;
+                    }
+                }
+            }
         }
     }
     a {
+        display: inline-flex;
+        align-items: center;
         text-decoration: none;
         color: $white;
+        .txt {
+            position: relative;
+            display: block;
+            padding: 1px 0;
+            &::before {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                border-bottom: 1px solid $white;
+                opacity: 0;
+            }
+        }
     }
+}
+
+.new {
+    display: inline-flex;
+    margin-right: 6px;
+    padding: 9px 6px 6px;
+    border-radius: 5px;
+    font-family: $ageo-bold;
+    font-size: 1.2rem;
+    line-height: 1;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    background: $secondary;
 }
 </style>
