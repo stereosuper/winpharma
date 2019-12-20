e94d566e7055662d734cbbf5585f0e2927660915
Clément Lemoine <clementlemoine.cl@gmail.com>
2019-12-20 17:30:11 +0100

    💄 testimonies

--- components/Testimonies.vue
+++ components/Testimonies.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="wrapper-testimonies">
+        <div class="wrapper-bg-testimonies">
+            <img src="img/bg-testimonies-mobile.svg" alt="" class="bg-testimonies-mobile" />
+            <img src="img/bg-testimonies.svg" alt="" class="bg-testimonies" />
+        </div>
         <div class="container">
             <div class="wrapper-title">
                 <h3 class="testimonies-title">
@@ -17,25 +21,31 @@
                 </Button>
             </div>
         </div>
-        <ul class="testimonies">
-            <li class="testimony">
-                <blockquote>
-                    <p>
-                        <span class="blockquote-content">
-                            Avec son clavier intuitif, <strong>Winpharma réfléchit et pense</strong> à la place de
-                            l’utilisateur&nbsp;!
-                        </span>
-                        <span class="blockquote-wrapper-author">
-                            <span class="wrapper-img" />
-                            <span>
-                                <cite class="blockquote-author">Hélène Lecoq</cite>
-                                <span class="blockquote-source">Issy-les-Moulineaux (92)</span>
+        <div class="around-testimonies">
+            <ul class="testimonies">
+                <li class="testimony">
+                    <blockquote>
+                        <h4 class="h4">
+                            Tranquillité d’esprit au quotidien
+                        </h4>
+                        <p>
+                            <span class="blockquote-content">
+                                WinAutopilote permet un pilotage automatique des commandes et une
+                                <strong>tranquillité d'esprit au quotidien.</strong> Je gagne 1h de temps par jour soit
+                                <strong>6h par semaine minimum.</strong>
                             </span>
-                        </span>
-                    </p>
-                </blockquote>
-            </li>
-        </ul>
+                            <span class="blockquote-wrapper-author">
+                                <span class="wrapper-img" />
+                                <span>
+                                    <cite class="blockquote-author">Hélène Lecoq</cite>
+                                    <span class="blockquote-source">Issy-les-Moulineaux (92)</span>
+                                </span>
+                            </span>
+                        </p>
+                    </blockquote>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -45,8 +55,31 @@ export default {};
 
 <style lang="scss" scoped>
 .wrapper-testimonies {
-    padding: 80px 0 120px;
+    position: relative;
+    padding: 120px 0;
 }
+.wrapper-bg-testimonies {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+}
+.bg-testimonies-mobile,
+.bg-testimonies {
+    position: absolute;
+    max-width: none;
+    left: -70px;
+    top: 3%;
+    width: 750px;
+    min-width: 100%;
+}
+
+.bg-testimonies {
+    display: none;
+}
+
 .wrapper-title {
     position: relative;
     margin-bottom: 30px;
@@ -56,7 +89,7 @@ export default {};
         position: absolute;
         bottom: 0;
         left: 0;
-        width: calc(100vw - #{2 * $small-gutter});
+        width: 100%;
         border-top: 1px solid #ffe2db;
     }
 }
@@ -64,17 +97,28 @@ export default {};
     font-family: $ageo-semi-bold;
     font-size: 2.8rem;
     line-height: 30px;
+    max-width: 480px;
 }
 .testimonies-intro {
     p {
         margin-bottom: 25px;
+        font-size: 1.6rem;
+        line-height: 28px;
+        color: $primary-darker;
     }
+}
+.testimonies {
+    margin: 40px 0 25px;
 }
 .testimony {
     padding: 20px 30px;
     border-radius: 8px;
     background: $white;
     box-shadow: 0px 0px 30px rgba(112, 112, 112, 0.1);
+    .h4 {
+        font-size: 2rem;
+        margin: 0 0 20px;
+    }
     .blockquote-content {
         font-family: $dm;
         font-size: 1.4rem;
@@ -111,6 +155,66 @@ export default {};
         margin-right: 15px;
         border-radius: 50%;
         background: $primary;
+    }
+}
+
+.around-testimonies {
+    padding: 0 $small-gutter * 2;
+}
+
+@media (min-width: $phone) {
+    .bg-testimonies-mobile,
+    .bg-testimonies {
+        left: 0;
+        top: 0;
+    }
+}
+
+@media (min-width: $tablet) {
+    .bg-testimonies-mobile {
+        display: none;
+    }
+    .bg-testimonies {
+        display: block;
+    }
+    .testimonies-intro {
+        width: percentage(8/12);
+        margin-left: percentage(2/12);
+    }
+    .testimonies-title {
+        font-size: 3.6rem;
+    }
+    .testimony {
+        padding: 40px 50px;
+        width: 400px;
+    }
+}
+
+@media (min-width: $desktop-small) {
+    .testimonies-title {
+        max-width: none;
+        width: percentage(5/12);
+    }
+    .wrapper-title {
+        margin-left: percentage(2/12);
+        &::before {
+            width: calc(100vw - #{2 * $small-gutter});
+        }
+    }
+    .testimonies-intro {
+        width: percentage(5/12);
+        margin-left: percentage(4/12);
+    }
+    .bg-testimonies {
+        top: auto;
+        bottom: 0;
+        width: 120%;
+    }
+}
+
+@media (min-width: $desktop-small) {
+    .bg-testimonies {
+        width: 100%;
     }
 }
 </style>
